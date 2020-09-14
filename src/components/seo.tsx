a1a8e6491ef18872b05a40bd97d803b467f6f07b
ziofat <printlts@gmail.com>
2020-09-15 02:09:50 +0800

    chore: add type for seo

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -10,9 +10,16 @@ import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
 
+interface SEOPorps {
+    description?: string;
+    lang?: string;
+    meta?: any;
+    title: string;
+}
+
 function SEO({
     description, lang, meta, title,
-}) {
+}: SEOPorps) {
     const { site } = useStaticQuery(
         graphql`
       query {
