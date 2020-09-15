0036a72a0b214ebbecd28b07e033517dd7fb08c3
ziofat <printlts@gmail.com>
2020-09-16 02:31:03 +0800

    feat: add result panel

--- src/core/components/edit.tsx
+++ src/core/components/edit.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Nav } from 'rsuite';
+import { Nav, FlexboxGrid } from 'rsuite';
 import { observer } from 'mobx-react';
 import { StoreProps, AppTab } from '../store';
 import EquipTab from './equip_tab';
@@ -23,16 +23,28 @@ export default class CoreEdit extends Component<StoreProps> {
                     left: 0,
                     right: 0,
                     bottom: 0,
-                    display: 'flex',
-                    flexDirection: 'column',
                 }}
             >
-                <Nav activeKey={store.tab} appearance="subtle" onSelect={this.switchTab} style={{ textAlign: 'right' }}>
-                    <Nav.Item eventKey={AppTab.EQUIP}>装备调整</Nav.Item>
-                    <Nav.Item eventKey={AppTab.CASE}>方案调整</Nav.Item>
-                </Nav>
-                { store.tab === AppTab.EQUIP && <EquipTab />}
-                { store.tab === AppTab.CASE && <CaseTab />}
+                <FlexboxGrid style={{ height: '100%' }}>
+                    <FlexboxGrid.Item colspan={18} style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+                        <Nav activeKey={store.tab} appearance="subtle" onSelect={this.switchTab} style={{ textAlign: 'right' }}>
+                            <Nav.Item eventKey={AppTab.EQUIP}>装备调整</Nav.Item>
+                            <Nav.Item eventKey={AppTab.CASE}>方案调整</Nav.Item>
+                        </Nav>
+                        { store.tab === AppTab.EQUIP && <EquipTab />}
+                        { store.tab === AppTab.CASE && <CaseTab />}
+                    </FlexboxGrid.Item>
+                    <FlexboxGrid.Item style={{ borderLeft: '1px solid #CCCCCC', height: '100%' }} colspan={6}>
+                        <Nav appearance="subtle">
+                            <Nav.Item icon={<i className="fal fa-undo" />} />
+                            <Nav.Item icon={<i className="fal fa-redo" />} />
+                            <Nav.Item icon={<i className="fal fa-save" />} />
+                            <Nav.Item icon={<i className="fal fa-folder-open" />} />
+                            <Nav.Item icon={<i className="fal fa-share-alt" />} />
+                        </Nav>
+                        result
+                    </FlexboxGrid.Item>
+                </FlexboxGrid>
             </main>
         );
     }
