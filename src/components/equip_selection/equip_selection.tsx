746deedf1b6b2f84ba70c3d5b58dcfeaa3388f41
ziofat <printlts@gmail.com>
2020-10-27 02:43:38 +0800

    fix: crash on cross kungfu equip

--- src/components/equip_selection/equip_selection.tsx
+++ src/components/equip_selection/equip_selection.tsx
@@ -218,7 +218,7 @@ export class EquipSelection extends Component<StoreProps, EquipSelectionState> {
                             </div>
                             <div>
                                 <i>
-                                    {item.tags
+                                    {(item.tags ?? [])
                                         .filter((t) => t !== 'huajing')
                                         .map((tag) => ATTRIBUTE_SHORT_DESC[tag])
                                         .join(' ')}
