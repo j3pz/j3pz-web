9b7ed2c5528c0f79646cdddbb1957d5a04793932
ziofat <printlts@gmail.com>
2020-10-30 13:38:30 +0800

    chore: add sourcemap

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,24 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const SentryWebpackPlugin = require('@sentry/webpack-plugin');
+
 exports.onCreateWebpackConfig = ({ actions, stage }) => {
     // If production JavaScript and CSS build
     if (stage === 'build-javascript') {
         // Turn off source maps
         actions.setWebpackConfig({
             devtool: false,
+            plugins: [
+                new SentryWebpackPlugin({
+                    // sentry-cli configuration
+                    authToken: process.env.SENTRY_AUTH_TOKEN,
+                    org: 'j3pz',
+                    project: 'j3pzweb',
+
+                    // webpack specific configuration
+                    include: 'src',
+                    ignore: ['node_modules'],
+                }),
+            ],
         });
     }
 };
