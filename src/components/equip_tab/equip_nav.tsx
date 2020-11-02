d08c7f7a2816dd37e15ed63656054171d0a623ca
ziofat <printlts@gmail.com>
2020-11-02 19:03:24 +0800

    feat: add equip preview

--- src/components/equip_tab/equip_nav.tsx
+++ src/components/equip_tab/equip_nav.tsx
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component, CSSProperties } from 'react';
 import { observer } from 'mobx-react';
-import { Sidenav, Nav } from 'rsuite';
+import { Sidenav, Nav, Whisper } from 'rsuite';
 import { StoreProps } from '../../store';
 import {
     Category, Position, CATEGORY_DESC, KungFu,
 } from '../../model/base';
 import './equip_nav.less';
+import { EquipView } from '../equip_view/equip_view';
+import { Equip } from '../../model/equip';
 
 interface NavInfo {
     category: Category;
@@ -26,6 +28,28 @@ export const navLib = new Map<Position, NavInfo>([
     [Position.PRIMARY_WEAPON, { category: Category.PRIMARY_WEAPON }],
     [Position.TERTIARY_WEAPON, { category: Category.TERTIARY_WEAPON }],
 ]);
+
+interface EquipOverlayProps {
+    style: CSSProperties;
+    equip?: Equip;
+}
+
+const EquipOverlay = React.forwardRef<HTMLDivElement, EquipOverlayProps>(({ style, equip, ...rest }: EquipOverlayProps, ref) => {
+    const styles: CSSProperties = {
+        ...style,
+        padding: 0,
+        minWidth: 300,
+        borderRadius: 4,
+        position: 'absolute',
+        boxShadow: '0 3px 6px -2px rgba(0, 0, 0, 0.6)',
+    };
+    return (
+        <div {...rest} style={styles} ref={ref}>
+            { equip ? <EquipView equip={equip} /> : null}
+        </div>
+    );
+});
+
 
 @observer
 export class EquipNav extends Component<StoreProps> {
@@ -57,17 +81,24 @@ export class EquipNav extends Component<StoreProps> {
                                 const name = CATEGORY_DESC[category];
                                 const equip = store.equips[key];
                                 return (
-                                    <Nav.Item
-                                        key={key}
-                                        eventKey={key}
-                                    >
-                                        <img
-                                            className="equip-icon"
-                                            src={equip?.icon
-                                                ? `https://icons.j3pz.com/${equip.icon}.png`
-                                                : `https://images.j3pz.com/imgs/icons/placeholder_${category}.png`}
-                                            alt={equip?.name ?? name}
-                                        />
+                                    <Nav.Item eventKey={key} key={key}>
+                                        <Whisper
+                                            delayHide={1}
+                                            trigger="hover"
+                                            speaker={(props, ref) => {
+                                                const { left, top } = props;
+                                                return <EquipOverlay style={{ left, top }} equip={equip} ref={ref} />;
+                                            }}
+                                            placement="auto"
+                                        >
+                                            <img
+                                                className="equip-icon"
+                                                src={equip?.icon
+                                                    ? `https://icons.j3pz.com/${equip.icon}.png`
+                                                    : `https://images.j3pz.com/imgs/icons/placeholder_${category}.png`}
+                                                alt={equip?.name ?? name}
+                                            />
+                                        </Whisper>
                                         <div className={equip?.id ? 'equip-type' : 'equip-full'}>{name}</div>
                                         { equip?.id && <div className="equip-name">{equip.name}</div> }
                                     </Nav.Item>
