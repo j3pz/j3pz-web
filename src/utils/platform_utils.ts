a85525870b02c6e5b858c1a04dd01f285fdf1dd9
ziofat <printlts@gmail.com>
2020-11-04 01:41:18 +0800

    fix: platform util build failed

--- src/utils/platform_utils.ts
+++ src/utils/platform_utils.ts
@@ -1,46 +1,28 @@
 export class PlatformUtil {
     // --------------   平台   ------------------
     public static isMobile(): boolean {
-        return /(mobile)/i.test(navigator.userAgent);
+        return /(mobile)/i.test(navigator?.userAgent ?? '');
     }
 
     public static isAndroid(): boolean {
-        return /(android)/i.test(navigator.userAgent);
+        return /(android)/i.test(navigator?.userAgent ?? '');
     }
 
     public static isIOS(): boolean {
-        return /(iphone|ipad|ipod)/i.test(navigator.userAgent);
+        return /(iphone|ipad|ipod)/i.test(navigator?.userAgent ?? '');
     }
 
     public static isIPad(): boolean {
-        return /(ipad)/i.test(navigator.userAgent);
+        return /(ipad)/i.test(navigator?.userAgent ?? '');
     }
 
     public static isMac(): boolean {
-        return !this.isMobile() && /Mac OS X/i.test(navigator.userAgent);
+        return !this.isMobile() && /Mac OS X/i.test(navigator?.userAgent ?? '');
     }
 
     // --------------   浏览器   ------------------
 
     public static isFireFox(): boolean {
-        return /firefox/i.test(navigator.userAgent);
-    }
-
-    // --------------   应用   ------------------
-
-    public static isWXorQQMiniProgram(): boolean {
-        return this.isWXMiniProgram() || this.isQQMiniProgram();
-    }
-
-    public static isWXMiniProgram(): boolean {
-        const wxUrlContainStr = '__wxjs_environment=miniprogram';
-        const index = window.location.href.indexOf(wxUrlContainStr);
-        return index > -1;
-    }
-
-    public static isQQMiniProgram(): boolean {
-        const wxUrlContainStr = '__wxjs_environment=qqminiprogram';
-        const index = window.location.href.indexOf(wxUrlContainStr);
-        return index > -1;
+        return /firefox/i.test(navigator?.userAgent ?? '');
     }
 }
