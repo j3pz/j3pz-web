be27308959c902f772c7e2bda957b0bf84096b02
ziofat <printlts@gmail.com>
2020-11-10 00:17:19 +0800

    fix: 调整 GA 选项

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,35 @@ module.exports = {
         author: '@魔术师胖叔叔',
     },
     plugins: [
+        {
+            resolve: 'gatsby-plugin-google-analytics',
+            options: {
+                // The property ID; the tracking code won't be generated without it
+                trackingId: 'UA-17530182-11',
+                // Defines where to place the tracking script - `true` in the head and `false` in the body
+                head: true,
+                // Setting this parameter is optional
+                anonymize: false,
+                // Setting this parameter is also optional
+                respectDNT: false,
+                // Avoids sending pageview hits from custom paths
+                exclude: [],
+                // Delays sending pageview hits on route update (in milliseconds)
+                pageTransitionDelay: 0,
+                // Enables Google Optimize using your container Id
+                // optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
+                // Enables Google Optimize Experiment ID
+                // experimentId: 'YOUR_GOOGLE_EXPERIMENT_ID',
+                // Set Variation ID. 0 for original 1,2,3....
+                // variationId: 'YOUR_GOOGLE_OPTIMIZE_VARIATION_ID',
+                // Defers execution of google analytics script after page load
+                defer: false,
+                // Any additional optional fields
+                sampleRate: 5,
+                siteSpeedSampleRate: 10,
+                cookieDomain: 'j3pz.com',
+            },
+        },
         'gatsby-plugin-react-helmet',
         {
             resolve: 'gatsby-source-filesystem',
@@ -47,35 +76,6 @@ module.exports = {
                     "Cannot read property 'nodeName' of null",
                     "Cannot read property 'getBoundingClientRect' of undefined",
                 ],
-            },
-        },
-        {
-            resolve: 'gatsby-plugin-google-analytics',
-            options: {
-                // The property ID; the tracking code won't be generated without it
-                trackingId: 'UA-17530182-11',
-                // Defines where to place the tracking script - `true` in the head and `false` in the body
-                head: true,
-                // Setting this parameter is optional
-                anonymize: true,
-                // Setting this parameter is also optional
-                respectDNT: true,
-                // Avoids sending pageview hits from custom paths
-                exclude: [],
-                // Delays sending pageview hits on route update (in milliseconds)
-                pageTransitionDelay: 0,
-                // Enables Google Optimize using your container Id
-                // optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
-                // Enables Google Optimize Experiment ID
-                // experimentId: 'YOUR_GOOGLE_EXPERIMENT_ID',
-                // Set Variation ID. 0 for original 1,2,3....
-                // variationId: 'YOUR_GOOGLE_OPTIMIZE_VARIATION_ID',
-                // Defers execution of google analytics script after page load
-                defer: false,
-                // Any additional optional fields
-                sampleRate: 5,
-                siteSpeedSampleRate: 10,
-                cookieDomain: 'j3pz.com',
             },
         },
         // this (optional) plugin enables Progressive Web App + Offline functionality
