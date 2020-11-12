0fc26a3f3da8e2989f9f65d594e7c3ef1c64790d
ziofat <printlts@gmail.com>
2020-11-13 01:16:50 +0800

    feat: add links

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -23,7 +23,7 @@ function Footer({ background = '#FAFAFA' }: FooterProps) {
                                 <Col className="link-list" xs={4}>
                                     <div className="list-header">关于</div>
                                     <Link to="about" className="list-item">这是什么</Link>
-                                    {/* <Link to="changelog" className="list-item">更新记录</Link> */}
+                                    <Link to="changelog" className="list-item">更新记录</Link>
                                     <Link to="credit" className="list-item">致谢</Link>
                                 </Col>
                                 <Col className="link-list" xs={4}>
@@ -37,8 +37,8 @@ function Footer({ background = '#FAFAFA' }: FooterProps) {
                                 </Col>
                                 <Col className="link-list" xs={4}>
                                     <div className="list-header">资源</div>
-                                    <div className="list-item">使用说明</div>
-                                    <div className="list-item">帮助与反馈</div>
+                                    <Link to="intro" className="list-item">使用说明</Link>
+                                    <a href="https://support.qq.com/products/105376" target="_blank" rel="noopener noreferrer" className="list-item">帮助与反馈</a>
                                     <Link to="policies/toc" className="list-item">用户协议</Link>
                                     <Link to="policies/privacy" className="list-item">隐私协议</Link>
                                 </Col>
@@ -51,9 +51,9 @@ function Footer({ background = '#FAFAFA' }: FooterProps) {
             <div className="infos content black">
                 <div className="social-media">
                     <a href="/"><img src="https://images.j3pz.com/imgs/wechat.png" alt="微信公众号" /></a>
-                    <a href="/"><img src="https://images.j3pz.com/imgs/weibo.png" alt="开发者微博" /></a>
+                    <a href="https://weibo.com/printlts" target="_blank" rel="noopener noreferrer"><img src="https://images.j3pz.com/imgs/weibo.png" alt="开发者微博" /></a>
                     <a href="/"><img src="https://images.j3pz.com/imgs/qq.png" alt="QQ 群组" /></a>
-                    <a href="/"><img src="https://images.j3pz.com/imgs/telegram.png" alt="Telegram 频道" /></a>
+                    <a href="https://t.me/j3pzapp" target="_blank" rel="noopener noreferrer"><img src="https://images.j3pz.com/imgs/telegram.png" alt="Telegram 频道" /></a>
                 </div>
                 <div className="legal">
                     <span>
